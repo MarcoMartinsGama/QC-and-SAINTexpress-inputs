e814cbc63f385c97b497fb34d175714c28b33246
MarcoMartinsGama <marcomartinsgama@gmail.com>
2024-06-20 10:27:47 -0700

    Add Generation of input files for SAINTexpress

--- server.R
+++ server.R
@@ -8,6 +8,7 @@ if (!requireNamespace("ComplexHeatmap", quietly = TRUE)) BiocManager::install("C
 if (!requireNamespace("ggplot2", quietly = TRUE))install.packages("ggplot2")
 if (!requireNamespace("readxl", quietly = TRUE))install.packages("readxl")
 if (!requireNamespace("shinyjs", quietly = TRUE))install.packages("shinyjs")
+if (!requireNamespace("seqinr", quietly = TRUE))install.packages("seqinr")
 library(BiocManager)
 library(artMS)
 library(MSstats)
@@ -16,8 +17,8 @@ library (ComplexHeatmap)
 library (ggplot2)
 library(readxl)
 library(shinyjs)
+library(seqinr)
 suppressPackageStartupMessages(library(ComplexHeatmap))
-
 
 function(input, output, session) {
   observeEvent(input$generate_qc, {
@@ -43,7 +44,6 @@ function(input, output, session) {
         )
       }
       
-      # Basic QC
       if (input$qc_basic == TRUE && !input$qc_ext) {
         artmsQualityControlEvidenceBasic(
           evidence_file = read.table(input$evidencefile$datapath, header = TRUE, sep = "\t"),
@@ -57,7 +57,6 @@ function(input, output, session) {
         })
       }
       
-      # Ext QC
       if (input$qc_ext == TRUE && !input$qc_basic) {
         artmsQualityControlEvidenceExtended(
           evidence_file = read.table(input$evidencefile$datapath, header = TRUE, sep = "\t"),
@@ -71,7 +70,6 @@ function(input, output, session) {
         })
       }
       
-      # Both QC(s)
       if (input$qc_basic == TRUE && input$qc_ext == TRUE) {
         output$output_text <- renderText({"Done."})
         artmsQualityControlEvidenceBasic(
@@ -90,9 +88,111 @@ function(input, output, session) {
         output$download_ui <- renderUI({
           tagList(
             downloadButton("download_basic", "Download Basic QC"),
-            downloadButton("download_extended", "Download Extended QC"))
+            downloadButton("download_extended", "Download Extended QC")
+          )
         })
       }
     })
   })
+  
+  observeEvent(input$generate_SAINT, {
+    req(input$evidencefile, input$keysfile, input$ref)
+    output$output_text2 <- renderText({"Working... Please Wait."})
+    
+    delay(10, {
+      quant_variable <- c()
+      if (input$msspc) quant_variable <- c(quant_variable, "msspc")
+      if (input$msint) quant_variable <- c(quant_variable, "msint")
+      
+      if (length(quant_variable) == 2) {
+        # Run with msspc
+        artmsEvidenceToSaintExpress(
+          evidence_file = input$evidencefile$datapath,
+          keys_file = input$keysfile$datapath,
+          ref_proteome_file = input$ref$datapath,
+          quant_variable = "msspc",
+          output_file = "output.txt"
+        )
+        
+        # Run with msint
+        artmsEvidenceToSaintExpress(
+          evidence_file = input$evidencefile$datapath,
+          keys_file = input$keysfile$datapath,
+          ref_proteome_file = input$ref$datapath,
+          quant_variable = "msint",
+          output_file = "output2.txt"
+        )
+        
+        # Merge interaction files
+        o1<- fread("output-saint-interactions.txt")
+        o2 <- fread("output2-saint-interactions.txt")
+        output_interactions <- cbind(o1,o2[[4]])
+        
+        
+        
+        fwrite(output_interactions, "output-saint-interactions.txt", sep = "\t")
+        
+        
+        output$output_text2 <- renderText({
+          "SAINTexpress files generated and merged successfully."
+        })
+        
+        output$download_ui2 <- renderUI({
+          tagList(
+            downloadButton("download_saint_interactions", "Download Merged Interactions"),
+            downloadButton("download_saint_baits", "Download SAINT Baits input file"),
+            downloadButton("download_saint_preys", "Download SAINT Preys input file")
+          )
+        })
+      } else {
+        artmsEvidenceToSaintExpress(
+          evidence_file = input$evidencefile$datapath,
+          keys_file = input$keysfile$datapath,
+          ref_proteome_file = input$ref$datapath,
+          quant_variable = quant_variable,
+          output_file = "output.txt"
+        )
+        
+        output$output_text2 <- renderText({
+          "SAINTexpress files generated successfully."
+        })
+        
+        output$download_ui2 <- renderUI({
+          tagList(
+            downloadButton("download_saint_interactions", "Download SAINT Interactions"),
+            downloadButton("download_saint_baits", "Download SAINT Baits input file"),
+            downloadButton("download_saint_preys", "Download SAINT Preys input file")
+          )
+        })
+      }
+    })
+  })
+  
+  output$download_saint_baits <- downloadHandler(
+    filename = function() {
+      "output-saint-baits.txt"
+    },
+    content = function(file) {
+      file.copy("output-saint-baits.txt", file)
+    }
+  )
+  
+  output$download_saint_preys <- downloadHandler(
+    filename = function() {
+      "output-saint-preys.txt"
+    },
+    content = function(file) {
+      file.copy("output-saint-preys.txt", file)
+    }
+  )
+  
+  output$download_saint_interactions <- downloadHandler(
+    filename = function() {
+      "output-saint-interactions.txt"
+    },
+    content = function(file) {
+      file.copy("output-saint-interactions.txt", file)
+    }
+  )
 }
+
--- ui.R
+++ ui.R
@@ -1,16 +1,25 @@
 library(shiny)
 fluidPage(
   useShinyjs(),
-  titlePanel("QC"),
+  titlePanel("QC and SAINT inputs"),
   sidebarLayout(
     sidebarPanel(
-      fileInput("evidencefile", "Input evidence.txt", accept = c("text/plain", ".txt")),
-      fileInput("keysfile", "Input keys.txt", accept = c("text/plain", ".txt")),
+      fileInput("evidencefile", "Upload evidence.txt", accept = c("text/plain", ".txt")),
+      fileInput("keysfile", "Upload keys.txt", accept = c("text/plain", ".txt")),
+      fileInput("ref", "Upload Reference proteome", accept = ".fasta")
+    ),
+    mainPanel(
       checkboxInput("qc_basic", "QC_Basic", value = TRUE),
       checkboxInput("qc_ext", "QC_Extended", value = FALSE),
       actionButton("generate_qc", "Generate QC(s)"),
       textOutput("output_text"),
-      uiOutput("download_ui")
-    ),
-    mainPanel()
-  ))
+      uiOutput("download_ui"),
+      checkboxInput("msspc", "msspc", value = TRUE),
+      checkboxInput("msint", "msint", value = FALSE),
+      actionButton("generate_SAINT", "Generate SAINTexpress input files"),
+      textOutput("output_text2"),
+      uiOutput("download_ui2")
+    )
+  )
+)
+
