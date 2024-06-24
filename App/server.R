26d734bb487960d0111508a7fdd5b20b6a37e58d
MarcoMartinsGama <marcomartinsgama@gmail.com>
2024-06-23 20:34:28 -0700

    V1.0

--- App/server.R
+++ App/server.R
@@ -3,7 +3,7 @@ library(shiny)
 # Option to allow big files like .fasta files
 options(shiny.maxRequestSize = 1000*1024^2)
 
-  # Install and load packages
+# Install and load packages
 if (!requireNamespace("BiocManager", quietly = TRUE))install.packages("BiocManager")
 if (!requireNamespace("MSstats", quietly = TRUE)) BiocManager::install("MSstats",force = TRUE)
 if (!requireNamespace("artMS", quietly = TRUE)) BiocManager::install("artMS",force = TRUE)
@@ -25,7 +25,7 @@ library(shinyjs)
 library(seqinr)
 
 function(input, output, session) {
-  # Generate Qc with button
+  # Generate QC with button
   observeEvent(input$generate_qc, {
     req(input$evidencefile, input$keysfile)
     output$output_text <- renderText({"Working... Please Wait."}) # Message for user patience 
@@ -35,6 +35,7 @@ function(input, output, session) {
         output$output_text <- renderText({"No QC(s) selected"})
         return()
       }
+      
       # Function to zip and download QC folders
       zip_and_download <- function(folder_name) {
         zipfile <- paste0(folder_name, ".zip")
@@ -56,7 +57,7 @@ function(input, output, session) {
           keys_file = read.table(input$keysfile$datapath, header = TRUE, sep = "\t"),
           prot_exp = "APMS"
         )
-    
+        
         output$download_basic <- zip_and_download("qc_basic")
         output$download_ui_basic <- renderUI({
           downloadButton("download_basic", "Download Basic QC")
@@ -69,12 +70,14 @@ function(input, output, session) {
           keys_file = read.table(input$keysfile$datapath, header = TRUE, sep = "\t"),
           plotPCA = input$perform_pca
         )
+        
         output$download_extended <- zip_and_download("qc_extended")
         output$download_ui_extended <- renderUI({
           downloadButton("download_extended", "Download Extended QC")
         })
       }
-         output$output_text <- renderText({"Done."}) # End message
+      
+      output$output_text <- renderText({"Done."}) # End message
     })
   })
   
@@ -85,37 +88,47 @@ function(input, output, session) {
     delay(10, { # Delay to display message correctly 
       if (input$msspc) { # Generate files with spectral count
         artmsEvidenceToSaintExpress(
-        evidence_file = input$evidencefile$datapath,
-        keys_file = input$keysfile$datapath,
-        ref_proteome_file = input$ref$datapath,
-        quant_variable = "msspc",
-        output_file = "msspc.txt")
+          evidence_file = input$evidencefile$datapath,
+          keys_file = input$keysfile$datapath,
+          ref_proteome_file = input$ref$datapath,
+          quant_variable = "msspc",
+          output_file = "msspc.txt"
+        )
         output$download_ui2 <- renderUI({ # UI to download spectral count files
           tagList(
             downloadButton("download_msspc_interactions", "Download msspc SAINT Interactions"),
-            downloadButton("download_msspc_baits", "Download msspc SAINT  Baits input file"),
-            downloadButton("download_msspc_preys", "Download msspc SAINT  Preys input file"))})}
+            downloadButton("download_msspc_baits", "Download msspc SAINT Baits input file"),
+            downloadButton("download_msspc_preys", "Download msspc SAINT Preys input file")
+          )
+        })
+      }
+      
       if (input$msint) { # Generate files with intensity
         artmsEvidenceToSaintExpress(
-        evidence_file = input$evidencefile$datapath,
-        keys_file = input$keysfile$datapath,
-        ref_proteome_file = input$ref$datapath,
-        quant_variable = "msint",
-        output_file = "msint.txt")
+          evidence_file = input$evidencefile$datapath,
+          keys_file = input$keysfile$datapath,
+          ref_proteome_file = input$ref$datapath,
+          quant_variable = "msint",
+          output_file = "msint.txt"
+        )
         output$download_ui2 <- renderUI({ # UI to download intensity files
           tagList(
             downloadButton("download_msint_interactions", "Download msint SAINT Interactions"),
             downloadButton("download_msint_baits", "Download msint SAINT Baits input file"),
-            downloadButton("download_msint_preys", "Download msint SAINT Preys input file"))})}
+            downloadButton("download_msint_preys", "Download msint SAINT Preys input file")
+          )
+        })
+      }
       
-      if(input$msint && input$msspc){ # Generate both with spectral count then intensity
+      if (input$msint && input$msspc) { # Generate both with spectral count then intensity
         # Run with msspc
         artmsEvidenceToSaintExpress(
           evidence_file = input$evidencefile$datapath,
           keys_file = input$keysfile$datapath,
           ref_proteome_file = input$ref$datapath,
           quant_variable = "msspc",
-          output_file = "msspc.txt")
+          output_file = "msspc.txt"
+        )
         
         # Run with msint
         artmsEvidenceToSaintExpress(
@@ -123,23 +136,27 @@ function(input, output, session) {
           keys_file = input$keysfile$datapath,
           ref_proteome_file = input$ref$datapath,
           quant_variable = "msint",
-          output_file = "msint.txt")
+          output_file = "msint.txt"
+        )
         output$download_ui2 <- renderUI({ # UI to download all files
           tagList(
             downloadButton("download_msspc_interactions", "Download msspc SAINT Interactions"),
-            downloadButton("download_msspc_baits", "Download msspc SAINT  Baits input file"),
-            downloadButton("download_msspc_preys", "Download msspc SAINT  Preys input file"),
+            downloadButton("download_msspc_baits", "Download msspc SAINT Baits input file"),
+            downloadButton("download_msspc_preys", "Download msspc SAINT Preys input file"),
             downloadButton("download_msint_interactions", "Download msint SAINT Interactions"),
             downloadButton("download_msint_baits", "Download msint SAINT Baits input file"),
             downloadButton("download_msint_preys", "Download msint SAINT Preys input file")
-          )})}
+          )
+        })
+      }
       
-        output$output_text2 <- renderText({ 
-          "SAINTexpress files generated successfully." # End message
-        }) 
-      })
+      output$output_text2 <- renderText({ 
+        "SAINTexpress files generated successfully." # End message
+      }) 
+    })
   })
-  # Download with buttons
+  
+  # Download handlers
   output$download_msspc_baits <- downloadHandler(
     filename = function() {
       "msspc-saint-baits.txt"
@@ -166,6 +183,7 @@ function(input, output, session) {
       file.copy("msspc-saint-interactions.txt", file)
     }
   )
+  
   output$download_msint_baits <- downloadHandler(
     filename = function() {
       "msint-saint-baits.txt"
@@ -193,4 +211,3 @@ function(input, output, session) {
     }
   )
 }
-
--- App/ui.R
+++ App/ui.R
@@ -3,40 +3,33 @@ fluidPage(
   titlePanel("QC and SAINT inputs generator"),
   sidebarLayout(
     sidebarPanel(
-      
       # Upload necessary files 
-      
       fileInput("evidencefile", "Upload evidence.txt", accept = c("text/plain", ".txt")),
       fileInput("keysfile", "Upload keys.txt", accept = c("text/plain", ".txt")),
       fileInput("ref", "Upload Reference proteome", accept = ".fasta")
     ),
     mainPanel(
-      
-          # Check if the users want to do basic, extended or both QC
+      # Check if the users want to do basic, extended or both QC
       checkboxInput("qc_basic", "QC_Basic", value = TRUE),
       checkboxInput("qc_ext", "QC_Extended", value = FALSE),
       conditionalPanel(
         condition = "input.qc_ext == true",
-        checkboxInput("perform_pca", "Perform PCA (Might take a long time, be patient)", value = FALSE)),
-      
-       # Button to generate QC and message confirming the work is in progress
+        checkboxInput("perform_pca", "Perform PCA (Might take a long time, be patient)", value = FALSE)
+      ),
+      # Button to generate QC and message confirming the work is in progress
       actionButton("generate_qc", "Generate QC(s)"),
       textOutput("output_text"),
-      
-        # Ui for downloading Qcs
+      # Ui for downloading QCs
       uiOutput("download_ui_basic"),
       uiOutput("download_ui_extended"),
-      
-        # Check boxes to generate input files for SAINTexpress in spectral count or intensity
+      # Check boxes to generate input files for SAINTexpress in spectral count or intensity
       checkboxInput("msspc", "msspc", value = TRUE),
       checkboxInput("msint", "msint", value = FALSE),
-        # Generate the files 
+      # Generate the files 
       actionButton("generate_SAINT", "Generate SAINTexpress input files"),
       textOutput("output_text2"),
-      
       # Download the files
       uiOutput("download_ui2")
     )
   )
 )
-
